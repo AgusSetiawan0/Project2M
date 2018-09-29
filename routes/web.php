b8230731fa4d3c2d19f64f94a1a3c04766960ce1
Agus Setiawan <37280475+AgusSetiawan0@users.noreply.github.com>
2018-09-29 14:41:36 +0700

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', 'ProjectController@welcome');
+
+Auth::routes(['verify' => true]);
+
+Route::group(['middleware' => ['admin']], function () {
+    Route::get('/admin', 'AdminController@adminDashboard');
+	Route::resource('projects', 'ProjectController')->only(['create']); //harus admin yang membuat project
+});
+
+// Projects
+Route::resource('projects', 'ProjectController')->only(['index', 'show']); // guest bisa melihat list project(index) dan info lengkap dari project(single)
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/admin', 'AdminController@adminDashboard');
+    Route::resource('users', 'adminController');
+	Route::resource('projects', 'ProjectController')->except(['index', 'show', 'create']); // user yang ter-auth bisa memakai fungsi CRUD nya. kecuali user biasa tidak bisa memakain fungsi create
+});
+
+
+
+// page yang bisa di lewati user tapi harus pake verifikasi email
+// Route::get('profile', function () {
+//     return 'dashboard';
+// })->middleware('verified');
+
+
+Route::get('/home', 'HomeController@index')->name('home');
