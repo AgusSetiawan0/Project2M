963edfacbaa95f9c1ce73f7e6aa9ece0ae4c5551
Agus Setiawan <37280475+AgusSetiawan0@users.noreply.github.com>
2018-09-30 14:11:59 +0700

    Add files via upload

--- database/migrations/2018_09_29_045925_create_topups_table.php
+++ database/migrations/2018_09_29_045925_create_topups_table.php
@@ -15,7 +15,16 @@ class CreateTopupsTable extends Migration
     {
         Schema::create('topups', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('nama');
+            $table->integer('nominal');
+            $table->string('bank');
+            $table->string('bukti_image');
+            $table->smallInteger('status')->default(0);
+            $table->dateTime('approved_at')->nullable();
+            $table->integer('user_id')->unsigned();
             $table->timestamps();
+            //foreign mengambil data di database lain
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
